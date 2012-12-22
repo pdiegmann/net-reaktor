a83197f2bf31c425b30a0a6d4fad2facdeccd056
Phil Diegmann <pdiegmann@gmail.com>
2012-12-22 18:53:47 +0100

    Merged net-reaktor and net-legacy-reaktor

--- reaktor/reaktorRequest.cs
+++ reaktor/reaktorRequest.cs
@@ -2,10 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 
 namespace reaktor
 {
     private class reaktorRequest
     {
+        protected String _url = String.Empty;
+        protected String _content = String.Empty;
+        protected String _method = "POST";
+        protected HttpWebRequest _request = null;
+
+        public reaktorRequest(String url, String content)
+            : base()
+        {
+            this._url = url;
+            this._content = content;
+        }
+
+        public reaktorRequest(String url, String content, String method)
+            : this(url, content)
+        {
+            this._method = method;
+        }
+
+        public void run()
+        {
+            _request = WebRequest.Create(_url) as HttpWebRequest;
+            _request.Method = _method;
+            _request.Accept = "application/json";
+
+            object data = new object();
+            IAsyncResult asr = _request.BeginGetResponse(new AsyncCallback(callback), null);
+        }
+
+        public void callback(IAsyncResult result) 
+        {
+            _request.EndGetResponse(result);
+        }
     }
 }
